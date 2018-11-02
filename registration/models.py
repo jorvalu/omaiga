523c8992e3529032fcf6bbb68f43e625562312f7
jorvalu <jorge.valencia@outlook.com>
2018-11-02 04:23:26 +0000

    Made email unique

--- registration/models.py
+++ registration/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 
 class User(AbstractUser):
-	karma = models.IntegerField(default=0)
+	email = models.EmailField(unique=True)
 
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
